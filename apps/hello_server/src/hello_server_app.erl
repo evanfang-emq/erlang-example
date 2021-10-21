38c821a81803c6f5fb0ae0103a4bfe433d51eef4
evan <evanfang@emq.com>
2021-10-21 15:41:25 +0800

    Add lager and customize console log color

--- apps/hello_server/src/hello_server_app.erl
+++ apps/hello_server/src/hello_server_app.erl
@@ -10,10 +10,13 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    % start lagger
+    lager:start(),
+    lager:info("server is listening on port 8080"),
     % server
     Dispatch = cowboy_router:compile([{'_', % must use SINGLE quote
-                                       [{<<"/">>, index_handler, []},
-                                        {<<"/db">>, db_handler, []}]}]),
+                                [{<<"/">>, index_handler, []},
+                                 {<<"/db">>, db_handler, []}]}]),
     {ok, _} = cowboy:start_clear(hello_listener,
                                  [{port, 8080}],
                                  #{env => #{dispatch => Dispatch}}),
